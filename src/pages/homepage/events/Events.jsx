8787857f6adc354d652b812c5f5000c5f190b696
Vatsal Varshney <vatsalvar1211@gmail.com>
2023-01-10 19:37:56 +0530

    Populated events data in homepage

--- src/pages/homepage/events/Events.jsx
+++ src/pages/homepage/events/Events.jsx
@@ -1,27 +1,19 @@
 import ul from "./heading-underline-vector.svg";
-// import sample from './events-sample.png'
 import { Swiper, SwiperSlide } from "swiper/react";
 import {
-  Virtual,
   Navigation,
   Pagination,
-  Scrollbar,
   A11y,
   Autoplay,
 } from "swiper";
 import "swiper/css";
-import "swiper/css/virtual";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import "swiper/css/scrollbar";
 import "swiper/css/autoplay";
-import { EventDataBase } from "../data/data";
 import data from "../../../data/events.json"
 import "./events.css";
 
 export default function Events() {
-  const slides = EventDataBase;
-
   return (
     <div className="events">
       <div className="eventsHeading">
@@ -30,27 +22,16 @@ export default function Events() {
       </div>
       <div className="eventsCarousel">
         <Swiper
-          modules={[Virtual, Navigation, Pagination, Scrollbar, A11y, Autoplay]}
-          // spaceBetween={10}
+          modules={[Navigation, Pagination, A11y, Autoplay]}
           slidesPerView={1.25}
           autoplay={{
             delay: 4000,
-            // pauseOnMouseEnter: true,
           }}
           centeredSlides={true}
           slideToClickedSlide={true}
-          // loop={true}
           navigation
           pagination={{ clickable: true }}
-          // virtual={{
-          //   slides: slides,
-          //   addSlidesAfter: slides.length,
-          //   addSlidesBefore: slides.length,
-          // }}
           breakpoints={{
-            // 1281: {
-            //     slidesPerView: 6,
-            // },
             1025: {
               slidesPerView: 5,
             },
@@ -107,46 +88,6 @@ export default function Events() {
               )}
             </SwiperSlide>
           ))}
-          {/* {slides.map((slideContent, index) => (
-            <SwiperSlide key={slideContent} virtualIndex={index}>
-              {({ isActive }) => (
-                <div
-                  className={
-                    isActive
-                      ? "card carouselCard carouselCardActive"
-                      : "card carouselCard carouselCardInactive"
-                  }
-                >
-                  <div className="card-img-top eventImg">
-                    <img src={slideContent.linkImg} alt="events" />
-                  </div>
-                  <div className="card-footer">
-                    {isActive ? (
-                      <div className="card-title">{slideContent.title}</div>
-                    ) : (
-                      <div className="card-title text-truncate">
-                        {slideContent.title}
-                      </div>
-                    )}
-                    <div className="card-text">
-                      <a
-                        className="eventReadMore"
-                        data-bs-toggle="collapse"
-                        href={`#collapse${index}`}
-                        aria-expanded="false"
-                        aria-controls={`#collapse${index}`}
-                      >
-                        Read more &raquo;
-                      </a>
-                      <div className="collapse" id={`collapse${index}`}>
-                        {slideContent.description}
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              )}
-            </SwiperSlide>
-          ))} */}
         </Swiper>
       </div>
     </div>
