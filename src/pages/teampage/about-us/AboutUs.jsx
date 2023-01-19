10581143230ef01dc54c4148eb2fe5259274a1e8
Vatsal Varshney <vatsalvar1211@gmail.com>
2023-01-19 21:41:25 +0530

    Grammatical corrections in About Us

--- src/pages/teampage/about-us/AboutUs.jsx
+++ src/pages/teampage/about-us/AboutUs.jsx
@@ -14,7 +14,7 @@ export default function AboutUs() {
                     <img src={il} alt=''/>
                 </div>
                 <div className="aboutUsTextbox">
-                    The Student Incubation Cell, IIT Delhi is formed with an intent to promote design thinking, planful ideation and support budding Entrepreneurs at IIT DELHI who can make a mark on the world map through their vision, next-generation technology, and services by leading from the front. Our mission is to empower every student in the campus in every way possible who aim high to translate their ideas into real-world Startup and to help in a smooth transition in every phase of this journey. As they say “It’s not about ideas, it’s about making ideas happen and we are committed to pave the way with every step one takes.
+                    The Student Incubation Cell, IIT Delhi is formed with an intent to promote design thinking, planful ideation and to support budding Entrepreneurs at IIT DELHI who can make a mark on the world map through their vision, next-generation technology, and services, by leading from the front. Our mission is to empower every student in the campus in every way possible who aim high to translate their ideas into real-world Startups and to help them in a smooth transition in every phase of this journey. As they say, “It’s not about ideas, it’s about making ideas happen" and we are committed to pave the way with every step one takes.
                 </div>
             </div>
         </div>
