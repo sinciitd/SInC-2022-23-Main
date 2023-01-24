f53a266ad0ed392430d7aaff8334e5fe0d92b2e9
SATYAPRAGNYA-KAR <satyakar2019@gmail.com>
2023-01-24 19:19:28 +0530

    Fixing Warnings to enable hosting on Netlify

--- src/pages/eventspage/Detail.js
+++ src/pages/eventspage/Detail.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Detail.css";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import Comment from "./Comment";
+// import Comment from "./Comment";
 
 function Detail({ event }) {
   return (
--- src/pages/teampage/teampage/Card.jsx
+++ src/pages/teampage/teampage/Card.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const Card = (props) => {
-    return (
-        <div className="d-flex flex-row justify-content-around align-items-center">
-            <div className="p-2 bd-highlight">
-                <div className="cardCoordi">
-                    <img className="card-img-topCoordi" src={props.img} alt={`${props.name} image`}></img>
-                    <div className="cardNameCoordi">
-                        <div><b>{props.name}</b></div>
-                        {props.position}
-                    </div>
-
-                </div>
+  return (
+    <div className="d-flex flex-row justify-content-around align-items-center">
+      <div className="p-2 bd-highlight">
+        <div className="cardCoordi">
+          <img
+            className="card-img-topCoordi"
+            src={props.img}
+            alt={`${props.name}`}
+          ></img>
+          <div className="cardNameCoordi">
+            <div>
+              <b>{props.name}</b>
             </div>
+            {props.position}
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
